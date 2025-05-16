9e9c813862e108cba9ab5b23c57322357e1739f8
scpcom <scpcom@gmx.de>
2025-05-17 00:38:44 +0200

    3rdparty: move TRD_MOD_PATH to 3rdparty.mk

--- 3rdparty/3rdparty.mk
+++ 3rdparty/3rdparty.mk
@@ -1,4 +1,5 @@
 # 3rdparty lib
+TRD_MOD_PATH = $(PWD)
 TRD_INCLUDE_PATH = $(realpath $(PWD)/..)/prebuilt/include
 TRD_LIB_PATH = $(realpath $(PWD)/..)/prebuilt/$(SDK_VER)
 TRD_TAR_PATH = $(realpath $(PWD)/..)/oss/oss_release_tarball/$(SDK_VER)
