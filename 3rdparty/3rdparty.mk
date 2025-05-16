ffdf83cef8d1f399b67c417dc90cf65c13b94e9b
scpcom <scpcom@gmx.de>
2025-05-17 00:34:04 +0200

    3rdparty: add SDK_VER to TRD_TMP_PATH

--- 3rdparty/3rdparty.mk
+++ 3rdparty/3rdparty.mk
@@ -2,4 +2,4 @@
 TRD_INCLUDE_PATH = $(realpath $(PWD)/..)/prebuilt/include
 TRD_LIB_PATH = $(realpath $(PWD)/..)/prebuilt/$(SDK_VER)
 TRD_TAR_PATH = $(realpath $(PWD)/..)/oss/oss_release_tarball/$(SDK_VER)
-TRD_TMP_PATH = $(realpath $(PWD)/..)/tmp
+TRD_TMP_PATH = $(realpath $(PWD)/..)/tmp/$(SDK_VER)
