71626410390064bc2c11c2733de6fd16d25a534e
scpcom <scpcom@gmx.de>
2025-05-16 23:59:36 +0200

    3rdparty: add 3rdparty.mk

---
+++ 3rdparty/3rdparty.mk
@@ -0,0 +1,5 @@
+# 3rdparty lib
+TRD_INCLUDE_PATH = $(realpath $(PWD)/..)/prebuilt/include
+TRD_LIB_PATH = $(realpath $(PWD)/..)/prebuilt/$(SDK_VER)
+TRD_TAR_PATH = $(realpath $(PWD)/..)/oss/oss_release_tarball/$(SDK_VER)
+TRD_TMP_PATH = $(realpath $(PWD)/..)/tmp
