deef5a2211f7035ae3ea63edcc0362dfc837d81d
scpcom <scpcom@gmx.de>
2025-05-28 17:16:48 +0200

    3rdparty: make unused modules optional

--- 3rdparty/3rdparty.mk
+++ 3rdparty/3rdparty.mk
@@ -4,3 +4,9 @@ TRD_INCLUDE_PATH = $(realpath $(PWD)/..)/prebuilt/include
 TRD_LIB_PATH = $(realpath $(PWD)/..)/prebuilt/$(SDK_VER)
 TRD_TAR_PATH = $(realpath $(PWD)/..)/oss/oss_release_tarball/$(SDK_VER)
 TRD_TMP_PATH = $(realpath $(PWD)/..)/tmp/$(SDK_VER)
+ifeq ($(OSS_TARBALL_REL),1)
+TRD_BUILD_TPUSDK_MODULE = build_3rdparty_module
+TRD_BUILD_OPTIONAL_MODULE = build_3rdparty_module
+else ifeq ($(TPU_REL),1)
+TRD_BUILD_TPUSDK_MODULE = build_3rdparty_module
+endif
