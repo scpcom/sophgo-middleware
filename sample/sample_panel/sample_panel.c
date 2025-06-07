94ccc89960383adcb869cf086542180817eaeb00
scpcom <scpcom@gmx.de>
2025-06-07 21:01:59 +0200

    Added support for the new 5-inch LCD (dxq5d0019_V0)

--- sample/sample_panel/sample_panel.c
+++ sample/sample_panel/sample_panel.c
@@ -58,6 +58,7 @@ typedef enum {
 	DSI_PANEL_ST7701,
 	DSI_PANEL_ST7701_D300FPC9307A,
 	DSI_PANEL_ST7701_DXQ5D0019B480854,
+	DSI_PANEL_ST7701_DXQ5D0019_V0,
 	DSI_PANEL_ST7701_HD228001C31,
 	DSI_PANEL_ZCT2133V1,
 	LVDS_PANEL_LCM185X56,
@@ -133,6 +134,7 @@ static char *s_panel_model_type_arr[] = {
 	"ST7701",
 	"ST7701_D300FPC9307A",
 	"ST7701_DXQ5D0019B480854",
+	"ST7701_DXQ5D0019_V0",
 	"ST7701_HD228001C31",
 	"ZCT2133V1",
 	"LCM185X56",
@@ -517,6 +519,13 @@ void SAMPLE_SET_PANEL_DESC(void)
 		g_panel_desc.stdsicfg.hs_timing_cfg = &hs_timing_cfg_st7701_480x854dxq;
 		g_panel_desc.stdsicfg.dsi_init_cmds = dsi_init_cmds_st7701_480x854dxq;
 		g_panel_desc.stdsicfg.dsi_init_cmds_size = ARRAY_SIZE(dsi_init_cmds_st7701_480x854dxq);
+		break;
+	case DSI_PANEL_ST7701_DXQ5D0019_V0:
+		g_panel_desc.panel_type = PANEL_MODE_DSI;
+		g_panel_desc.stdsicfg.dev_cfg = &dev_cfg_st7701_480x854dxq_V0;
+		g_panel_desc.stdsicfg.hs_timing_cfg = &hs_timing_cfg_st7701_480x854dxq_V0;
+		g_panel_desc.stdsicfg.dsi_init_cmds = dsi_init_cmds_st7701_480x854dxq_V0;
+		g_panel_desc.stdsicfg.dsi_init_cmds_size = ARRAY_SIZE(dsi_init_cmds_st7701_480x854dxq_V0);
 		break;
 	case DSI_PANEL_ST7701_HD228001C31:
 		g_panel_desc.panel_type = PANEL_MODE_DSI;
--- sample/sample_panel/sample_panel.h
+++ sample/sample_panel/sample_panel.h
@@ -30,6 +30,7 @@ extern "C" {
 #include "dsi_st7701.h"
 #include "dsi_st7701_d300fpc9307a.h"
 #include "dsi_st7701_dxq5d0019b480854.h"
+#include "dsi_st7701_dxq5d0019_V0.h"
 #include "dsi_st7701_hd228001c31.h"
 #include "dsi_zct2133v1.h"
 #include "dsi_hx8399_1080p.h"
